c2e53c523e7e7e326e923218638e7b042387d676
Caleb Denio <cjdenio44@gmail.com>
2023-04-05 16:41:16 -0400

    Update drawing.mjs

--- drawing.mjs
+++ drawing.mjs
@@ -3,6 +3,9 @@ export default function drawing(drawing) {
 
   drawing.strokeStyle = "red";
   drawing.strokeRect(50, 50, 50, 50);
+  
+  drawing.strokeStyle = "blue";
+  drawing.strokeRect(100, 100, 100, 100);
 
   /* STOP DRAWING HERE */
 }
